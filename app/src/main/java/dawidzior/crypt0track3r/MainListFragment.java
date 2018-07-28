3c128397af8f82b09a335532cdd6dadeeb9d5f71
Dawid <klembix@o2.pl>
2018-07-28 11:58:27 +0200

    Deleted unnecessary default constructor

--- app/src/main/java/dawidzior/crypt0track3r/MainListFragment.java
+++ app/src/main/java/dawidzior/crypt0track3r/MainListFragment.java
@@ -42,10 +42,6 @@ public class MainListFragment extends Fragment {
     @BindView(R.id.crypto_list)
     RecyclerView cryptoRecyclerView;
 
-    public MainListFragment() {
-        // Required empty public constructor
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main_list, container, false);
